a42bb31051b00340dbbdecc57b68fd7a0dfb831b
MAGEEK\Jindra <jindra@mageek.cz>
2020-06-20 20:56:14 +0200

    Rename extension directory

--- app/code/community/Mageek/OpenmageTheme/Helper/Data.php
+++ app/code/community/Mageek/OpenmageTheme/Helper/Data.php
@@ -2,16 +2,16 @@
 
 /**
  * @category    Mageek
- * @package     Mageek_AdminTheme
+ * @package     Mageek_OpenmageTheme
  * @author      Jindra Hejlík <jindra@hejlik.cz>
  * @copyright   Copyright (c) Mageek (http://mageek.cz/)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Mageek_AdminTheme_Helper_Data extends Mage_Core_Helper_Data
+class Mageek_OpenmageTheme_Helper_Data extends Mage_Core_Helper_Data
 {
 
-    const CONFIG_XML_PATH_THEME = 'admin/mageek_admintheme/theme_name';
+    const CONFIG_XML_PATH_THEME = 'admin/mageek_openmagetheme/theme_name';
 
     public function getThemeName()
     {
--- app/code/community/Mageek/OpenmageTheme/Model/Observer.php
+++ app/code/community/Mageek/OpenmageTheme/Model/Observer.php
@@ -2,19 +2,19 @@
 
 /**
  * @category    Mageek
- * @package     Mageek_AdminTheme
+ * @package     Mageek_OpenmageTheme
  * @author      Jindra Hejlík <jindra@hejlik.cz>
  * @copyright   Copyright (c) Mageek (http://mageek.cz/)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Mageek_AdminTheme_Model_Observer
+class Mageek_OpenmageTheme_Model_Observer
 {
 
     public function selectedTheme()
     {
 
-        $theme = Mage::helper('mageek_admintheme')->getThemeName();
+        $theme = Mage::helper('mageek_openmagetheme')->getThemeName();
 
         if (!empty($theme)) {
             Mage::getDesign()->setArea('adminhtml')->setTheme($theme);
