7f401b5fcc1ba9075dfdfb298e4fc9f17fcd16a4
MAGEEK\Jindra <jindra@hejlik.cz>
2021-04-17 15:30:34 +0200

    update theme switcher

---
+++ app/code/community/Mageek/OpenmageTheme/Block/Adminhtml/Dashboard/Graph.php
@@ -0,0 +1,272 @@
+<?php
+
+/**
+ * @category    Mageek
+ * @package     Mageek_OpenmageTheme
+ * @author      Jindra Hejlík <jindra@hejlik.cz>
+ * @copyright   Copyright (c) Mageek (http://hejlik.cz/)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+class Mageek_OpenmageTheme_Block_Adminhtml_Dashboard_Graph extends Mage_Adminhtml_Block_Dashboard_Graph
+{
+    /**
+     * Chart width
+     *
+     * @var string
+     */
+    // Set the width to 722px
+    protected $_width = '722';
+
+    /**
+     * Chart height
+     *
+     * @var string
+     */
+    // Set the height to 722px
+    protected $_height = '400';
+
+    /**
+     * Get chart url
+     *
+     * @param bool $directUrl
+     * @return string
+     */
+    public function getChartUrl($directUrl = true)
+    {
+    	// Set the bar chart options
+        $params = array(
+            'cht'  => 'bvs',
+            'chf'  => 'bg,s,F5F7F9|b0,lg,90,FF006A,1,9F089B,0.4',
+            'chm'  => 'o,DF0482,0,-1,6.0|B,FCECF4,0,0,0',
+            'chco' => 'DF0482',
+            'chxs' => '0,0,11|1,0,11',
+            'chma' => '20,20,50,10',
+            'chls' => '2'
+        );
+
+        $this->_allSeries = $this->getRowsData($this->_dataRows);
+
+        foreach ($this->_axisMaps as $axis => $attr){
+            $this->setAxisLabels($axis, $this->getRowsData($attr, true));
+        }
+
+        $timezoneLocal = Mage::app()->getStore()->getConfig(Mage_Core_Model_Locale::XML_PATH_DEFAULT_TIMEZONE);
+
+        list ($dateStart, $dateEnd) = Mage::getResourceModel('reports/order_collection')
+            ->getDateRange($this->getDataHelper()->getParam('period'), '', '', true);
+
+        $dateStart->setTimezone($timezoneLocal);
+        $dateEnd->setTimezone($timezoneLocal);
+
+        $dates = array();
+        $datas = array();
+
+        while($dateStart->compare($dateEnd) < 0){
+            switch ($this->getDataHelper()->getParam('period')) {
+                case '24h':
+                    $d = $dateStart->toString('yyyy-MM-dd HH:00');
+                    $dateStart->addHour(1);
+                    break;
+                case '7d':
+                case '1m':
+                    $d = $dateStart->toString('yyyy-MM-dd');
+                    $dateStart->addDay(1);
+                    break;
+                case '1y':
+                case '2y':
+                    $d = $dateStart->toString('yyyy-MM');
+                    $dateStart->addMonth(1);
+                    break;
+            }
+            foreach ($this->getAllSeries() as $index=>$serie) {
+                if (in_array($d, $this->_axisLabels['x'])) {
+                    $datas[$index][] = (float)array_shift($this->_allSeries[$index]);
+                } else {
+                    $datas[$index][] = 0;
+                }
+            }
+            $dates[] = $d;
+        }
+
+        /**
+         * setting skip step
+         */
+        if (count($dates) > 8 && count($dates) < 15) {
+            $c = 1;
+        } else if (count($dates) >= 15){
+            $c = 2;
+        } else {
+            $c = 0;
+        }
+        /**
+         * skipping some x labels for good reading
+         */
+        $i=0;
+        foreach ($dates as $k => $d) {
+            if ($i == $c) {
+                $dates[$k] = $d;
+                $i = 0;
+            } else {
+                $dates[$k] = '';
+                $i++;
+            }
+        }
+
+        $this->_axisLabels['x'] = $dates;
+        $this->_allSeries = $datas;
+
+        // Image-Charts Awesome data format values
+        $params['chd'] = "a:";
+        $dataDelimiter = ",";
+        $dataSetdelimiter = "|";
+        $dataMissing = "_";
+
+        // process each string in the array, and find the max length
+        foreach ($this->getAllSeries() as $index => $serie) {
+            $localmaxlength[$index] = count($serie);
+            $localmaxvalue[$index] = max($serie);
+            $localminvalue[$index] = min($serie);
+        }
+
+        if (is_numeric($this->_max)) {
+            $maxvalue = $this->_max;
+        } else {
+            $maxvalue = max($localmaxvalue);
+        }
+        if (is_numeric($this->_min)) {
+            $minvalue = $this->_min;
+        } else {
+            $minvalue = min($localminvalue);
+        }
+
+        // default values
+        $yrange = 0;
+        $yLabels = array();
+        $miny = 0;
+        $maxy = 0;
+        $yorigin = 0;
+
+        $maxlength = max($localmaxlength);
+        if ($minvalue >= 0 && $maxvalue >= 0) {
+            $miny = 0;
+            if ($maxvalue > 10) {
+                $p = pow(10, $this->_getPow($maxvalue));
+                $maxy = (ceil($maxvalue/$p))*$p;
+                $yLabels = range($miny, $maxy, $p);
+            } else {
+                $maxy = ceil($maxvalue+1);
+                $yLabels = range($miny, $maxy, 1);
+            }
+            $yrange = $maxy;
+            $yorigin = 0;
+        }
+
+        $chartdata = array();
+
+        foreach ($this->getAllSeries() as $index => $serie) {
+            $thisdataarray = $serie;
+            for ($j = 0; $j < count($thisdataarray); $j++) {
+                $currentvalue = $thisdataarray[$j];
+                if (is_numeric($currentvalue)) {
+                    $ylocation = $yorigin + $currentvalue;
+                    array_push($chartdata, $ylocation . $dataDelimiter);
+                } else {
+                    array_push($chartdata, $dataMissing . $dataDelimiter);
+                }
+            }
+            array_push($chartdata, $dataSetdelimiter);
+        }
+        $buffer = implode('', $chartdata);
+
+        $buffer = rtrim($buffer, $dataSetdelimiter);
+        $buffer = rtrim($buffer, $dataDelimiter);
+        $buffer = str_replace(($dataDelimiter . $dataSetdelimiter), $dataSetdelimiter, $buffer);
+
+        $params['chd'] .= $buffer;
+
+        $labelBuffer = "";
+        $valueBuffer = array();
+        $rangeBuffer = "";
+
+        if (count($this->_axisLabels)) {
+            $params['chxt'] = implode(',', array_keys($this->_axisLabels));
+            $indexid = 0;
+            foreach ($this->_axisLabels as $idx=>$labels){
+                if ($idx == 'x') {
+                    /**
+                     * Format date
+                     */
+                    foreach ($this->_axisLabels[$idx] as $_index=>$_label) {
+                        if ($_label != '') {
+                            switch ($this->getDataHelper()->getParam('period')) {
+                                case '24h':
+                                    $this->_axisLabels[$idx][$_index] = $this->formatTime(
+                                        new Zend_Date($_label, 'yyyy-MM-dd HH:00'), 'short', false
+                                    );
+                                    break;
+                                case '7d':
+                                case '1m':
+                                    $this->_axisLabels[$idx][$_index] = $this->formatDate(
+                                        new Zend_Date($_label, 'yyyy-MM-dd')
+                                    );
+                                    break;
+                                case '1y':
+                                case '2y':
+                                    $formats = Mage::app()->getLocale()->getTranslationList('datetime');
+                                    $format = isset($formats['yyMM']) ? $formats['yyMM'] : 'MM/yyyy';
+                                    $format = str_replace(array("yyyy", "yy", "MM"), array("Y", "y", "m"), $format);
+                                    $this->_axisLabels[$idx][$_index] = date($format, strtotime($_label));
+                                    break;
+                            }
+                        } else {
+                            $this->_axisLabels[$idx][$_index] = '';
+                        }
+
+                    }
+
+                    $tmpstring = implode('|', $this->_axisLabels[$idx]);
+
+                    $valueBuffer[] = $indexid . ":|" . $tmpstring;
+                    if (count($this->_axisLabels[$idx]) > 1) {
+                        $deltaX = 100/(count($this->_axisLabels[$idx])-1);
+                    } else {
+                        $deltaX = 100;
+                    }
+                } else if ($idx == 'y') {
+                    $valueBuffer[] = $indexid . ":|" . implode('|', $yLabels);
+                    if (count($yLabels)-1) {
+                        $deltaY = 100/(count($yLabels)-1);
+                    } else {
+                        $deltaY = 100;
+                    }
+                    // setting range values for y axis
+                    $rangeBuffer = $indexid . "," . $miny . "," . $maxy . "|";
+                }
+                $indexid++;
+            }
+            $params['chxl'] = implode('|', $valueBuffer);
+        }
+
+        // chart size
+        $params['chs'] = $this->getWidth().'x'.$this->getHeight();
+
+        if (isset($deltaX) && isset($deltaY)) {
+            $params['chg'] = $deltaX . ',' . $deltaY . ',1,0';
+        }
+
+        // return the encoded data
+        if ($directUrl) {
+            $p = array();
+            foreach ($params as $name => $value) {
+                $p[] = $name . '=' .urlencode($value);
+            }
+            return self::API_URL . '?' . implode('&', $p);
+        } else {
+            $gaData = urlencode(base64_encode(json_encode($params)));
+            $gaHash = Mage::helper('adminhtml/dashboard_data')->getChartDataHash($gaData);
+            $params = array('ga' => $gaData, 'h' => $gaHash);
+            return $this->getUrl('*/*/tunnel', array('_query' => $params));
+        }
+    }
+}
